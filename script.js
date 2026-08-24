8197bf1e19eb196fe27e00f9e19069b99ae64723
azedinelatoui19-oss <azedinelatoui19@gmail.com>
2026-08-24 20:12:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ✅ الرابط الفعلي
-const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzhbXrAO0ojiPaDmCPaWBszoo2wvj9XjN1sHiOHLIFLs8lRh7K-__Qj6Tvr5amq85ko/exec";
+const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx73DV9AnzfPwyiaSK0sZzF2yaHrh-eTpKCkohqwU6o0snmGGlyIMV7PcHBmuFg1-bq/exec";
 const ADMIN_PHONE = "0655787605"; // الرقم الإداري
 
 // العناصر
